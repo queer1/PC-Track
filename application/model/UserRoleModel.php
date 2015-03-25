7a82493045b5d58ede46a4f21d2086cd238df23a
Nicolaj "Roe" Rørvig <Roervig2473@gmail.com>
2015-03-25 22:33:27 +0100

    Update UserRoleModel.php

--- application/model/UserRoleModel.php
+++ application/model/UserRoleModel.php
@@ -6,16 +6,19 @@
  * This class contains everything that is related to up- and downgrading accounts.
  */
 class UserRoleModel {
+    public static $addPermQuery = null;
+    public static $getPermsQuery = null;
+    public static $removePermQuery = null;
     /**
      * @param $user_id
      * @param $new_perm
      */
     public static function addPerm($user_id, $new_perm) {
+        if(self::$addPermQuery === null){
+            self::$addPermQuery = DatabaseFactory::getFactory()->getConnection()->prepare("UPDATE users SET perms = :new WHERE user_id = :user_id")
+        }
         $newset = array_push($new_perm, UserRoleModel::getPerms($user_id));
-        $database = DatabaseFactory::getFactory()->getConnection();
-        $sql = "UPDATE users SET perms = :new WHERE user_id = :user_id";
-        $query = $database->prepare($sql);
-        $query->execute(array(':user_id' => $user_id, ':new' => json_encode($newset)));
+        self::$addPermQuery->execute(array(':user_id' => $user_id, ':new' => json_encode($newset)));
     }
 
     /**
@@ -24,11 +27,10 @@ class UserRoleModel {
      * @return array $perms
      */
     public static function getPerms($user_id) {
-        $database = DatabaseFactory::getFactory()->getConnection();
-        $sql = "SELECT user_permissions
-                FROM users WHERE user_id = :user_id LIMIT 1";
-        $query = $database->prepare($sql);
-        $query->execute(array(':user_id' => $user_id));
+        if(self::$getPermsQuery === null){
+            self::$getPermsQuery = DatabaseFactory::getFactory()->getConnection()->prepare("SELECT user_permissions FROM users WHERE user_id = :user_id LIMIT 1");
+        }
+        self::$getPermsQuery->execute(array(':user_id' => $user_id));
 
         $perms = json_decode($query->fetch(), true);
         return $perms;
@@ -39,12 +41,12 @@ class UserRoleModel {
      * @param $removed_perm
      */
     public static function removePerm($user_id, $removed_perm) {
+        if(self::$removePermQuery === null){
+            self::$removePermQuery = DatabaseFactory::getFactory()->getConnection()->prepare("UPDATE users SET perms = :new WHERE user_id = :user_id");
+        }
         $original = UserRoleModel::getPerms($user_id);
         $being_removed = array_search($removed_perm, $original);
         unset($original[$being_removed]);
-        $database = DatabaseFactory::getFactory()->getConnection();
-        $sql = "UPDATE users SET perms = :new WHERE user_id = :user_id";
-        $query = $database->prepare($sql);
-        $query->execute(array(':new' => json_encode($original), ':user_id' => $user_id));
+        self::$removePermQuery->execute(array(':new' => json_encode($original), ':user_id' => $user_id));
     }
 }
