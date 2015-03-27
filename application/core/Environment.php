98a42fe109773270296c600aaf97568db6cce517
tankerkiller125 <tankerkiller125@gmail.com>
2015-03-27 18:21:43 -0400

    Autodetect Enviroment?

--- application/core/Environment.php
+++ application/core/Environment.php
@@ -7,10 +7,20 @@
  * Extend this the way you want.
  */
 class Environment {
-    public static function get() {
+    public static function getApache() {
         // if APPLICATION_ENV constant exists (set in Apache configs)
         // then return content of APPLICATION_ENV
         // else return "development"
         return (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : "development");
+    }
+
+    public static function get() {
+        if($_SERVER['REMOTE_ADDR'] == "::1" || $_SERVER['REMOTE_ADDR'] == "127.0.0.1") {
+            return "development";
+        } elseif($_SERVER['HTTP_X_FORWARDED_FOR'] == "::1" || $_SERVER['HTTP_X_FORWARDED_FOR'] == "127.0.0.1") {
+            return "development";
+        } else {
+            return "production";
+        }
     }
 }
